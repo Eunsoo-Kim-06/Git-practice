3f12de4ce290a4ddbb591fa1e95a1c2ee0953ed6
Eunsoo-Kim-06 <sksmsrladmstnek@gmail.com>
2026-05-22 10:50:35 +0900

    commit: "Add result print function"

--- grades.py
+++ grades.py
@@ -27,6 +27,7 @@ def calculate_average(scores):
         return 0.0
     return sum(scores.values()) / len(scores)
 
+def print_result(scores, average):
 
 if __name__ == "__main__":
     scores = get_scores()
